ba40aaa09ec0b564c64f41a94cff646b54323a50
Anonymous commit robot <commit.robot@digital.cabinet-office.gov.uk>
2014-09-01 15:26:16 +0100

    6 hours of swearing about Python code

--- captain_web.py
+++ captain_web.py
@@ -19,9 +19,11 @@ DOCKER_API_VERSION = os.getenv("DOCKER_API_VERSION")
 def before_request():
     g.captain_conn = Connection(nodes=DOCKER_NODES, api_version=DOCKER_API_VERSION)
 
+
 class RestApplications(restful.Resource):
     def get(self):
         return g.captain_conn.get_applications()
+
 
 class RestApplication(restful.Resource):
     def get(self, application_name):
@@ -30,8 +32,26 @@ class RestApplication(restful.Resource):
         except KeyError:
             abort(404)
 
+    def delete(self, application_name):
+        g.captain_conn.stop_application(application_name)
+        pass
+
+
+class RestApplicationInstance(restful.Resource):
+    def get(self, application_name, instance_id):
+        try:
+            #TODO
+            return g.captain_conn.get_applications()[application_name]
+        except KeyError:
+            abort(404)
+
+    def delete(self, application_name, instance_id):
+        #TODO
+        pass
+
 api.add_resource(RestApplications, '/apps/')
 api.add_resource(RestApplication, '/apps/<string:application_name>')
+api.add_resource(RestApplicationInstance, '/apps/<string:application_name>/instances/<string:instance_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=1234)
