8370898c5ebd6fb8f47fd979ce3b0a95465aceb0
Anonymous commit robot <commit.robot@digital.cabinet-office.gov.uk>
2014-09-02 14:51:12 +0100

    Removed 'address' from the REST API

--- captain/tests/test_model.py
+++ captain/tests/test_model.py
@@ -10,16 +10,14 @@ class TestInstance(unittest.TestCase):
         app = "paye"
         version = "1.2.0"
         node = "appserver-1"
-        address = "192.168.17.6"
         port = 43127
 
         # when
-        instance = Instance(id, app, version, node, address, port)
+        instance = Instance(id, app, version, node, port)
 
         # then
         self.assertEqual(id, instance["id"])
         self.assertEqual(app, instance["app"])
         self.assertEqual(version, instance["version"])
         self.assertEqual(node, instance["node"])
-        self.assertEqual(address, instance["address"])
         self.assertEqual(port, instance["port"])
