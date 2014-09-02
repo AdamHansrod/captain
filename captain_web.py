ed0499f5e3322956cd31ab8167efa79c9aedf1e6
Anonymous commit robot <commit.robot@digital.cabinet-office.gov.uk>
2014-09-02 14:19:28 +0100

    Switched the REST API to operate in terms of 'instances'

--- captain_web.py
+++ captain_web.py
@@ -20,37 +20,24 @@ def before_request():
     g.captain_conn = Connection(nodes=DOCKER_NODES, api_version=DOCKER_API_VERSION)
 
 
-class RestApplications(restful.Resource):
+class RestInstances(restful.Resource):
     def get(self):
-        return g.captain_conn.get_applications()
+        return g.captain_conn.get_instances()
 
 
-class RestApplication(restful.Resource):
-    def get(self, application_name):
+class RestInstance(restful.Resource):
+    def get(self, instance_id):
         try:
-            return g.captain_conn.get_applications()[application_name]
+            return filter(lambda instance: instance["id"] == instance_id, g.captain_conn.get_instances())[0]
         except KeyError:
             abort(404)
 
-    def delete(self, application_name):
-        g.captain_conn.stop_application(application_name)
+    def delete(self, instance_id):
+        g.captain_conn.stop_instance(instance_id)
         return '', 204
 
-
-class RestApplicationInstance(restful.Resource):
-    def get(self, application_name, application_instance_id):
-        try:
-            return filter(lambda application_instance: application_instance["id"] == application_instance_id, g.captain_conn.get_applications()[application_name])[0]
-        except KeyError:
-            abort(404)
-
-    def delete(self, application_name, application_instance_id):
-        g.captain_conn.stop_application_instance(application_name, application_instance_id)
-        return '', 204
-
-api.add_resource(RestApplications, '/apps/')
-api.add_resource(RestApplication, '/apps/<string:application_name>')
-api.add_resource(RestApplicationInstance, '/apps/<string:application_name>/instances/<string:application_instance_id>')
+api.add_resource(RestInstances, '/instances/')
+api.add_resource(RestInstance, '/instances/<string:instance_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=1234)
