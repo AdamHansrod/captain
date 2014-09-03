6cccaeaf8f2873da3fa7f3237f8417c33c311bbb
Anonymous commit robot <commit.robot@digital.cabinet-office.gov.uk>
2014-09-03 11:50:58 +0100

    Unit test for starting instances added

--- captain/tests/test_connection.py
+++ captain/tests/test_connection.py
@@ -1,7 +1,9 @@
 import unittest
+import uuid
 from mock import patch
 from captain.config import Config
 from captain.connection import Connection
+from captain.model import Instance
 from captain.tests.util_mock import ClientMock
 
 
@@ -10,7 +12,7 @@ class TestConnection(unittest.TestCase):
     def setUp(self):
         self.config = Config()
         self.config.docker_nodes = ["http://node-1/", "http://node-2/"]
-        self.config.slug_path = "http://hostname/{app_name}-{app_version}-slug.tgz"
+        self.config.slug_path = "http://host/{app_name}-{app_version}-slug.tgz"
         self.config.slug_runner_command = "runner command"
         self.config.slug_runner_image = "runner/image"
 
@@ -55,6 +57,49 @@ class TestConnection(unittest.TestCase):
         self.assertEqual(2, instance3["environment"].__len__())
         self.assertEqual("-Dapplication.log=INFO -Drun.mode=Prod -Dlogger.resource=/application-json-logger.xml -Dhttp.port=8080", instance3["environment"]["HMRC_CONFIG"])
         self.assertEqual("-Xmx256m -Xms256m", instance3["environment"]["JAVA_OPTS"])
+
+    @patch('docker.Client')
+    @patch('uuid.uuid4')
+    def test_starts_instance(self, uuid_mock, docker_client):
+        # given
+        (mock_client_node1, mock_client_node2) = ClientMock().mock_two_docker_nodes(docker_client)
+        uuid_mock.return_value = 'SOME-UUID'
+        instance_to_start = Instance(None, "paye", "216", "node-1", None,
+                                     {
+                                         'HMRC_CONFIG': "-Dapplication.log=INFO -Drun.mode=Prod -Dlogger.resource=/application-json-logger.xml -Dhttp.port=8080",
+                                         'JAVA_OPTS': "-Xmx256m -Xms256m"
+                                     })
+
+        # when
+        connection = Connection(self.config)
+        started_instance = connection.start_instance(instance_to_start)
+
+        # then
+        self.assertEqual("eba8bea2600029", started_instance["id"])
+        self.assertEqual("paye", started_instance["app"])
+        self.assertEqual("216", started_instance["version"])
+        self.assertEqual("node-1", started_instance["node"])
+        self.assertEqual(9317, started_instance["port"])
+        self.assertEqual(2, started_instance["environment"].__len__())
+        self.assertEqual("-Dapplication.log=INFO -Drun.mode=Prod -Dlogger.resource=/application-json-logger.xml -Dhttp.port=8080", started_instance["environment"]["HMRC_CONFIG"])
+        self.assertEqual("-Xmx256m -Xms256m", started_instance["environment"]["JAVA_OPTS"])
+
+        mock_client_node1.create_container.assert_called_with(image=self.config.slug_runner_image,
+                                                              command=self.config.slug_runner_command,
+                                                              ports=[8080],
+                                                              environment={
+                                                                  'PORT': '8080',
+                                                                  'SLUG_URL': 'http://host/paye-216-slug.tgz',
+                                                                  'HMRC_CONFIG': '-Dapplication.log=INFO -Drun.mode=Prod -Dlogger.resource=/application-json-logger.xml -Dhttp.port=8080',
+                                                                  'JAVA_OPTS': '-Xmx256m -Xms256m'
+                                                              },
+                                                              detach=True,
+                                                              name="paye_216_SOME-UUID")
+
+        mock_client_node1.start.assert_called_with("eba8bea2600029", port_bindings={8080: None})
+
+        self.assertFalse(mock_client_node2.create_container.called)
+        self.assertFalse(mock_client_node2.start.called)
 
     @patch('docker.Client')
     def test_stops_instance(self, docker_client):
