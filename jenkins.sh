a28270b9ce6c7a9cb9114b75429c908abb4b4ee6
Tom van Neerijnen <tom@tomvn.com>
2014-09-08 15:14:20 +0100

    moved to quicker jenkins.sh as default test runner

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 V_ENV_NAME="captain-venv"
-virtualenv ${V_ENV_NAME}
+if [ ! -d ${V_ENV_NAME} ] ;  then
+    virtualenv ${V_ENV_NAME}
+fi
 source "${V_ENV_NAME}/bin/activate"
 
-echo "Doing Pip Installs"
-pip install -q -r requirements.txt
-pip install -q -r requirements-tests.txt
+diff <(sort requirements.txt requirements-tests.txt | grep -v ^"#") <(pip freeze | sort)
+if [ $? != 0 ] ; then
+  echo "Doing Pip Installs"
+  pip install --upgrade -q -r requirements.txt
+  pip install --upgrade -q -r requirements-tests.txt
+fi
 rm -fr target
 mkdir target
 nosetests --with-xunit --xunit-file=target/nosetests.xml --with-xcover --xcoverage-file=target/coverage/coverage.xml --cover-package=captain --cover-erase --cover-html-dir=target/coverage --cover-html
 
 deactivate
-rm -fr ${V_ENV_NAME}
