12848b50432bad3524c52acb6863bfad206a6afa
Edd Grant <edd.grant@digital.hmrc.gov.uk>
2017-06-21 11:08:37 +0100

    PBD-37 - Re-implement tests using moto instead of Boto Stubber.

--- captain/aws_host_resolver.py
+++ captain/aws_host_resolver.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class AWSHostResolver(object):
 
     def __init__(self, ec2_client, dry_run=False, max_results=1000):
@@ -5,11 +8,13 @@ class AWSHostResolver(object):
         self.dry_run = dry_run
         self.max_results = max_results
 
-    def find_running_hosts_by_tag(self, tag_name, tag_value):
+    def find_running_hosts_private_ip_by_tag(self, tag_name, tag_value):
         """
         Returns a list of private IP address strings
         for any running hosts with the specified tag.
         """
+        logging.debug("Looking for EC2 hosts with tag: {} and value: {}".format(tag_name, tag_value))
+
         tag_filter = {
             'Name': 'tag:{}'.format(tag_name),
             'Values': [tag_value]
@@ -22,6 +27,12 @@ class AWSHostResolver(object):
             Filters=[tag_filter, running_host_filter],
             DryRun=self.dry_run
         )
-        instances = describe_instances_response['Reservations'][0]['Instances']
-        # TODO: Add logging of the instance id and IP address.
+
+        logging.debug("Describe Instances Response: {}".format(describe_instances_response))
+
+        if len(describe_instances_response['Reservations']) > 0:
+            instances = describe_instances_response['Reservations'][0]['Instances']
+        else:
+            instances = []
+
         return [instance['PrivateIpAddress'] for instance in instances]
--- captain/tests/test_aws_host_resolver.py
+++ captain/tests/test_aws_host_resolver.py
@@ -1,63 +1,55 @@
 import unittest
 
-from botocore.session import Session
-from botocore.stub import Stubber
+import boto3
+from moto import mock_ec2
 
 from captain.aws_host_resolver import AWSHostResolver
-
-#TODO: Look at using https://github.com/spulec/moto
 
 
 class TestAWSHostResolver(unittest.TestCase):
 
     def setUp(self):
-        #TODO: Figure out how to set up boto so it doesn't want MFA during tests.
-        self.session = Session(profile='service-prototypes-engineer')
-        self.ec2_client = self.session.create_client('ec2')
-        self.ec2_client_stubber = Stubber(self.ec2_client)
+        self.ec2_resource = boto3.resource('ec2')
+        self.ec2_client = self.ec2_resource.meta.client
         self.under_test = AWSHostResolver(self.ec2_client)
 
-    def test_it_should_resolve_hosts_from_tags(self):
+    @mock_ec2
+    def test_it_should_return_private_ip_addresses_of_tagged_hosts(self):
         # Given
         tag_name = 'role'
         tag_value = 'applicationservers'
 
-        private_ip_addresses = ['172.31.14.146', '172.31.14.147']
+        untagged_instances = self.ec2_resource.create_instances(ImageId='some-ami-id', MinCount=2, MaxCount=2)
+        tagged_instances = self.ec2_resource.create_instances(ImageId='some-ami-id', MinCount=2, MaxCount=2)
 
-        expected_response = {
-            'Reservations': [
-                {
-                    'Instances': [
-                        {'InstanceId': 'i-0fb9bc94c497fc22b', 'PrivateIpAddress': private_ip_addresses[0]},
-                        {'InstanceId': 'i-0fb9bc94c497fc22c', 'PrivateIpAddress': private_ip_addresses[1]}
-                    ]
-                }
-            ]
-        }
-
-        expected_params = {
-            'Filters': [
-                {
-                    'Name': 'tag:role',
-                    'Values': [
-                        tag_value,
-                    ]
-                },
-                {
-                    'Name': 'instance-state-name',
-                    'Values': ['running']
-                }
-            ],
-            'DryRun': False
-        }
-
-        self.ec2_client_stubber.add_response('describe_instances',
-                                             expected_response,
-                                             expected_params)
-        self.ec2_client_stubber.activate()
+        for instance in tagged_instances:
+            instance.create_tags(
+                Tags=[
+                    {
+                        'Key': tag_name,
+                        'Value': tag_value
+                    }
+                ]
+            )
 
         # When
-        actual_response = self.under_test.find_running_hosts_by_tag(tag_name, tag_value)
+        actual_response = self.under_test.find_running_hosts_private_ip_by_tag(tag_name, tag_value)
 
         # Then
-        self.assertEqual(private_ip_addresses, actual_response)
+        self.assertTrue(2, len(actual_response))
+        for untagged_instance in untagged_instances:
+            self.assertNotIn(untagged_instance.private_ip_address, actual_response)
+        for tagged_instance in tagged_instances:
+            self.assertIn(tagged_instance.private_ip_address, actual_response)
+
+    @mock_ec2
+    def test_it_should_return_an_empty_list_when_no_hosts_are_matched(self):
+        # Given
+        tag_name = 'role'
+        tag_value = 'applicationservers'
+
+        # When
+        actual_response = self.under_test.find_running_hosts_private_ip_by_tag(tag_name, tag_value)
+
+        # Then
+        self.assertLessEqual([], actual_response)
